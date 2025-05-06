337f030ec506f935976b399eccd5ed855dd48459
ashashas-alt <mcomqueijo8@gmail.com>
2025-05-06 22:27:55 +0000

    salvando progresso antes de resolver conflitos

--- .vscode/Super_Trunfo.c
+++ .vscode/Super_Trunfo.c
@@ -11,11 +11,11 @@
 
 int main (){
     
-    // VARIÁVEIS CARTA 1
-    char estado1[50];                       //%c
+  // VARIÁVEIS CARTA 1   
+    unsigned long populacao1;              
+    char estado1[50];                   //%c
     char codigo1[4];                    // Ex:"A01"
-    char cidade1[40];
-    int populacao1;                     //%d
+    char cidade1[40];                   //%d
     float area1;                        //%f
     float pib1;
     int pontos_turisticos1;             //%d
@@ -24,44 +24,54 @@ int main (){
     char estado2[50];
     char codigo2[4];
     char cidade2[40];
-    int populacao2;
+    unsigned long populacao2; 
     float area2;
     float pib2;
     int pontos_turisticos2;
 
-    //CARTA 1
+  //CARTA 1
     printf("\n==CADASTRO CARTA 1==\n");
-    printf("\nEstado: ");
+    printf("\n Estado (A-H): ");
     scanf(" %49s", &estado1);
-    printf("\nCodigo: ");
+    printf("\n Codigo (ex: A01): ");
     scanf("%3s", &codigo1);
-    printf("\ncidade: ");
+    printf("\n cidade: ");
     scanf("%39s", &cidade1);
-    printf("\npopulação: ");
-    scanf("%d", &populacao1);
-    printf("\nArea km²: ");
+    printf("\n população: ");
+    scanf("%lu", &populacao1);
+    printf("\n Area (km²): ");
     scanf("%f",&area1);
-    printf("\nPib: ");
+    printf("\n PIB (em bilhões): ");
     scanf("%f",&pib1);
-    printf("\nPontos turisticos: ");
+    printf("\n Pontos turisticos: ");
     scanf("%d",&pontos_turisticos1);
+ 
+  
+  //CARTA 2
+  printf("\n==CADASTRO CARTA 2==\n");
+  printf("\n Estado (A-H): ");
+  scanf(" %49s", &estado2);
+  printf("\n Codigo (ex: A01): ");
+  scanf("%3s", &codigo2);
+  printf("\n cidade: ");
+  scanf("%39s", &cidade2);
+  printf("\n população: ");
+  scanf("%lu", &populacao2);
+  printf("\n Area (km²): ");
+  scanf("%f",&area2);
+  printf("\n PIB (em bilhões): ");
+  scanf("%f",&pib2);
+  printf("\n Pontos turisticos: ");
+  scanf("%d",&pontos_turisticos2);
+   
 
-    //cadastro carta 2      '
-    printf("\n==CADASTRO CARTA 2==\n");
-    printf("\nEstado: ");
-    scanf(" %49s", &estado2);
-    printf("\nCodigo: ");
-    scanf("%3s", &codigo2);
-    printf("\ncidade: ");
-    scanf("%39s", &cidade2);
-    printf("\npopulação: ");
-    scanf("%d", &populacao2);
-    printf("\nArea km²: ");
-    scanf("%f",&area2);
-    printf("\nPib: ");
-    scanf("%f",&pib2);
-    printf("\nPontos turisticos: ");
-    scanf("%d",&pontos_turisticos2);
+// =====Nível aventureiro=====
+//cálculos para carta 1
+   densidade1 = populacao1 / area1;
+    pib_per_capita1 = (pib1 * 1000000000)/populacao1;
+    //cálculos para carta 2
+densidade2 = populacao2 / area2;
+pib_per_capita2 = (pib2 * 1000000000)/populacao2;
 
     //RESULTADO 
     //carta 1
